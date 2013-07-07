251c51324ca972d59addd7cdbdee67af239f3eef
percentcer <spence.miller@gmail.com>
2013-07-07 09:41:54 +0000

    initial commit

---
+++ names.js
@@ -0,0 +1,85 @@
+#!/usr/bin/env node
+
+var http = require("http");
+
+function getDomainrReq( someWord ) {
+    return {
+        host   : "domai.nr",
+        port   : 80,
+        path   : "/api/json/search?q=" + someWord
+    };
+};
+
+function getThesReq( someWord ) {
+    return {
+        host : "words.bighugelabs.com",
+        path : "/api/2/18445d364105eed58a7ddd788f64078c/" + someWord + "/json",
+        port : 80
+    };
+};
+
+function printDomainsForWord( someWord ) {
+    http.get(
+        getDomainrReq( someWord ),
+        function ( res ) {
+            var acc = '';
+
+            res.setEncoding('utf8');
+
+            res.on( 'data', function ( c ) { acc += c;} );
+
+            res.on( 'end', function () {
+                var obj = JSON.parse(acc);
+                for (var _i = 0, _len = obj['results'].length; _i < _len; _i++) {
+                    var domObj = obj['results'][_i];
+                    if (domObj.availability == "available") {
+                        console.log( domObj.domain );
+                        }
+                    }
+                }
+            );
+        });
+};
+
+function processSynonyms( res ) {
+    var theJSON = '';
+
+    res.setEncoding('utf8');
+
+    res.on('data', 
+            function ( c ) { 
+                theJSON += c; 
+            }
+    );
+
+    res.on('end', function () {
+        if (theJSON == '') { console.error("no synonyms found for " + process.argv[2]); return; }
+
+        var obj = JSON.parse(theJSON);
+        for (var pos in obj) {
+            for (var rel in obj[pos]) {
+                for (var _i = 0, _len = obj[pos][rel].length; _i < _len; _i++) {
+                    var theWord = obj[pos][rel][_i];
+                    if ( !Boolean(theWord.match(/\W/g) ) ) {
+                        printDomainsForWord( obj[pos][rel][_i] );
+                    }
+                }
+            }
+        }
+    });
+};
+
+function getDomains( someWord ) {
+    printDomainsForWord( someWord );
+    http.get(
+        getThesReq( someWord ),
+        processSynonyms
+    );
+}
+
+if (require.main == module) {
+    getDomains(process.argv[2]);
+}
+
+exports.getDomains = getDomains;
+
